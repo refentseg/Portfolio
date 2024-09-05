db282bd49eb1959dd71cc11fd072e05710966c7b
theuglybwe <refentse.dev@gmail.com>
2024-09-05 21:28:20 +0200

    Minor Visual changes

--- client/src/app/Layout/Footer.tsx
+++ client/src/app/Layout/Footer.tsx
@@ -13,11 +13,12 @@ const XIcon = createLucideIcon("X", [
   ]);
 
 export default function Footer(){
+  const currentYear = new Date().getFullYear();
 return(
 <footer className="bg-black border-t border-gray-400 py-4">
      <div className="container mx-auto flex justify-between items-center px-4">
         <p className="text-sm text-gray-300">
-        © 2024 All rights reserved.
+        © {currentYear} All rights reserved.
         </p>
         <div className="flex space-x-4">
             <a href="https://www.linkedin.com/in/rgaonnwe/" target='_blank' className="text-gray-200 hover:text-gray-500"><Linkedin size={24}/></a>
