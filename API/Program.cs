4b09b265e0be8bd19c779fb6d3bf2703403dca99
theuglybwe <refentse.dev@gmail.com>
2024-09-07 22:41:22 +0200

    Removed cache

--- API/Program.cs
+++ API/Program.cs
@@ -43,8 +43,34 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-builder.Services.AddDbContext<ProjectContext>(opt =>{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+string connStr;
+if (builder.Environment.IsDevelopment()){
+    connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+// Use connection string provided at runtime by FlyIO.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    Console.WriteLine("DATABASE_URL: " + connUrl);
+
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+    var updatedHost = pgHost.Replace("flycast", "internal");
+
+    connStr = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    
+}
+builder.Services.AddDbContext<ProjectContext>(opt =>
+{
+    opt.UseNpgsql(connStr); 
 });
 
 
